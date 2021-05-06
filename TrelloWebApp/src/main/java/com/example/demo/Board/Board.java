a11eab00d40ebdee15b38ccb50138f3d1212c995
dinh <dinhhung.0115@gmail.com>
2021-05-06 08:45:57 +0700

    update DB

---
+++ TrelloWebApp/src/main/java/com/example/demo/Board/Board.java
@@ -0,0 +1,79 @@
+package com.example.demo.Board;
+
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import com.example.demo.Card.Visiable;
+import com.example.demo.User.User;
+
+@Entity
+@Table(name = "board")
+public class Board {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, unique = true, length = 45)
+	private Long id;
+	
+	@Column(name = "title", nullable = false, length = 45)
+	private String title;
+	
+	@Column(name = "backGroundImage", nullable = true, length = 45)
+	private String backGroundImage;
+	
+	@Enumerated(EnumType.STRING)
+    @Column(name = "visiable")
+    private Visiable visiable = Visiable.PUBLIC;
+	
+	@ManyToMany
+	private Set<User> users;
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getBackGroundImage() {
+		return backGroundImage;
+	}
+
+	public void setBackGroundImage(String backGroundImage) {
+		this.backGroundImage = backGroundImage;
+	}
+
+	public Visiable getVisiable() {
+		return visiable;
+	}
+
+	public void setVisiable(Visiable visiable) {
+		this.visiable = visiable;
+	}
+
+	public Set<User> getUsers() {
+		return users;
+	}
+
+	public void setUsers(Set<User> users) {
+		this.users = users;
+	}
+}
---
+++ TrelloWebApp/src/main/java/com/example/demo/ListOfCard/ListOfCardRepo.java
@@ -0,0 +1,7 @@
+package com.example.demo.ListOfCard;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ListOfCardRepo extends JpaRepository<ListOfCard, Long>{
+
+}
