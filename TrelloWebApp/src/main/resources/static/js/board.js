914d86822812ddcd3f196a95c577e3e4d15878e5
kimhuegg <huelt1610@gmail.com>
2021-05-12 16:58:14 +0700

    update board+home

--- TrelloWebApp/src/main/resources/static/js/board.js
+++ TrelloWebApp/src/main/resources/static/js/board.js
@@ -283,3 +283,40 @@ function drop(e) {
 
 // On Load
 updateDOM();
+
+// onclick button
+var notification_click = document.getElementById("notification-click")
+console.log(notification_click);
+notification_click.addEventListener('click',function(){
+    var modal_notification = document.getElementById("modal-notification")
+    // console.log(notification_click)
+    modal_notification.classList.toggle('open-modal-notification');
+});
+
+var avatar_click = document.getElementById("user-avatar")
+console.log(avatar_click);
+avatar_click.addEventListener('click',function(){
+    var modal_notification = document.getElementById("modal-user-info")
+    // console.log(notification_click)
+    modal_notification.classList.toggle('open-modal-user-info');
+});
+
+var list_item_team_click = document.getElementById("menu-extend");
+console.log(list_item_team_click);
+list_item_team_click.addEventListener('click',function(){
+    var list_item_extend = document.querySelector("#list-iteam-team > ul:nth-child(2)");
+    // console.log(list_item_team_click)
+    // list_item_extend.style.backgroundColor = "red";
+    list_item_extend.classList.toggle('ul-extend');
+});
+
+// open edit form
+function openEditForm() {
+  var edit_form = document.getElementById("form-edit-board")
+  edit_form.classList.toggle('display-none');
+
+}
+function closeEditForm(){
+  var edit_form = document.getElementById("form-edit-board")
+  edit_form.classList.toggle('display-none');
+}
